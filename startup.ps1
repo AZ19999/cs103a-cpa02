22d3ebbe3394d9ae28d198947c12f7d991350919
AZ19999 <zhualexander@yahoo.com>
2022-05-08 22:33:21 -0400

    url changed

---
+++ startup.ps1
@@ -0,0 +1,2 @@
+$Env.mongodb_URI = 'mongodb+srv://AlexZhu:Baccus23!@cluster0.smai9.mongodb.net/sample_restaurants?retryWrites=true&w=majority'
+nodemon
---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export mongodb_URI='mongodb+srv://AlexZhu:Baccus23!@cluster0.smai9.mongodb.net/sample_restaurants?retryWrites=true&w=majority'
+echo "connecting to $mongodb_URI"
+nodemon
