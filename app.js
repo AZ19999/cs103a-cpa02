4b220669409a88e108fd37f03a8fed4036998e3b
AZ19999 <zhualexander@yahoo.com>
2022-05-08 22:05:42 -0400

    reverted a change

--- app.js
+++ app.js
@@ -35,15 +35,8 @@ const mongoose = require( 'mongoose' );
 const mongodb_URI = 'mongodb+srv://AlexZhu:Baccus23!@cluster0.smai9.mongodb.net/sample_restaurants?retryWrites=true&w=majority'
 
 
-//mongoose.connect(mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
+mongoose.connect(mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-  () => {
-    console.log('Connected to MongoDB');
-  }
-);
 // fix deprecation warnings
 mongoose.set('useFindAndModify', false); 
 mongoose.set('useCreateIndex', true);
